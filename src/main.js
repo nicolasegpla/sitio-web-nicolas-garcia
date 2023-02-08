f533b168ff75708bf10a9ca71bb0dace3901907e
nicolasegpla <nicolasedixongarciapineros@gmail.com>
2023-02-07 20:10:59 -0500

    add dropdown with functionality

--- src/main.js
+++ src/main.js
@@ -1,20 +1,11 @@
-function myFunction() {
-    if(mobileVersionDropdownMenu.style.display === 'grid') {
-        mobileVersionDropdownMenu.style.display = 'none';
-    }else {
-        mobileVersionDropdownMenu.style.display = 'none';
-    }
-}
+sectionHome1MenuHamburguesa.addEventListener('click', () => {
+    mobileVersionDropdownMenu.classList.toggle('transformYMenuMovil');
+});
 
-divMenuDesplegableImgBotonCierre.addEventListener('click', myFunction);
-window.addEventListener('scroll', myFunction);
+divMenuDesplegableImgBotonCierre.addEventListener('click', () => {
+    mobileVersionDropdownMenu.classList.toggle('transformYMenuMovil');
+});
+window.addEventListener('scroll', () => {
+    mobileVersionDropdownMenu.classList.remove('transformYMenuMovil');
+});
 
-function myFunction2() {
-    if(mobileVersionDropdownMenu.style.display === 'none') {
-        mobileVersionDropdownMenu.style.display = 'grid';
-    }else {
-        mobileVersionDropdownMenu.style.display = 'grid';
-    }
-}
-
-sectionHome1MenuHamburguesa.addEventListener('click', myFunction2);
